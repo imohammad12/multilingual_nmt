41b963c39b47bac148d9fa646102bfcf10d112f9
mohammad dehghan <mohamad.dehghan12@yahoo.com>
2020-02-05 02:36:22 +0330

    synced to google colab

--- tools/bpe_pipeline_bilingual.sh
+++ tools/bpe_pipeline_bilingual.sh
@@ -32,29 +32,29 @@ echo "Name : = $NAME"
 echo "Step 1a: Preprocess inputs"
 
 echo "Learning BPE on source and target combined"
-cat "${TRAIN_SRC}" "${TRAIN_TGT}" | learn_bpe -s ${BPE_OPS} > $OUT/data/bpe-codes.${BPE_OPS}
+#cat "${TRAIN_SRC}" "${TRAIN_TGT}" | learn_bpe -s ${BPE_OPS} > $OUT/data/bpe-codes.${BPE_OPS}
 
 echo "Applying BPE on source"
-apply_bpe -c "$OUT/data/bpe-codes.${BPE_OPS}" < "$TRAIN_SRC" > "$OUT/data/train.src"
-apply_bpe -c "$OUT/data/bpe-codes.${BPE_OPS}" < "$VALID_SRC" > "$OUT/data/valid.src"
-apply_bpe -c "$OUT/data/bpe-codes.${BPE_OPS}" < "$TEST_SRC" > "$OUT/data/test.src"
+#apply_bpe -c "$OUT/data/bpe-codes.${BPE_OPS}" < "$TRAIN_SRC" > "$OUT/data/train.src"
+#apply_bpe -c "$OUT/data/bpe-codes.${BPE_OPS}" < "$VALID_SRC" > "$OUT/data/valid.src"
+#apply_bpe -c "$OUT/data/bpe-codes.${BPE_OPS}" < "$TEST_SRC" > "$OUT/data/test.src"
 
 echo "Applying BPE on target"
-apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} <  "$TRAIN_TGT" > $OUT/data/train.tgt
-apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} <  "$VALID_TGT" > $OUT/data/valid.tgt
+#apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} <  "$TRAIN_TGT" > $OUT/data/train.tgt
+#apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} <  "$VALID_TGT" > $OUT/data/valid.tgt
 # We dont touch the test References, No BPE on them!
-cp "$TEST_TGT" "$OUT/data/test.tgt"
+#cp "$TEST_TGT" "$OUT/data/test.tgt"
 
 echo "Step 1b: Preprocess"
-python3 "${TF}/preprocess.py" -i "${OUT}/data" \
-      -s-train train.src \
-      -t-train train.tgt \
-      -s-valid valid.src \
-      -t-valid valid.tgt \
-      -s-test test.src \
-      -t-test test.tgt \
-      --save_data processed \
-      --max_seq_len 70
+#python3 "${TF}/preprocess.py" -i "${OUT}/data" \
+#      -s-train train.src \
+#      -t-train train.tgt \
+#      -s-valid valid.src \
+#      -t-valid valid.tgt \
+#      -s-test test.src \
+#      -t-test test.tgt \
+#      --save_data processed \
+#      --max_seq_len 70
 
 echo "Step 2: Train"
 TF2=$(printf "%q\n" "$(pwd)")
@@ -67,21 +67,21 @@ CMD="python3 $TF2/train.py -i $OUT/data --data processed \
 
 # --gpu "$GPUARG"
 
-echo "Training command :: $CMD"
-eval "$CMD"
-
-echo "BPE decoding/detokenising target to match with references"
-mv $OUT/test/test.out{,.bpe}
-mv $OUT/test/valid.out{,.bpe}
-cat $OUT/test/valid.out.bpe | sed -E 's/(@@ )|(@@ ?$)//g' > "$OUT/test/valid.out"
-cat $OUT/test/test.out.bpe | sed -E 's/(@@ )|(@@ ?$)//g' > "$OUT/test/test.out"
-
-echo "Step 4a: Evaluate Test"
-perl "$TF/tools/multi-bleu.perl" "$OUT/data/test.tgt" < "$OUT/test/test.out" > "$OUT/test/test.tc.bleu"
-perl "$TF/tools/multi-bleu.perl" -lc "$OUT/data/test.tgt" < "$OUT/test/test.out" > "$OUT/test/test.lc.bleu"
-
-echo "Step 4b: Evaluate Dev"
-perl "$TF/tools/multi-bleu.perl" "$OUT/data/valid.tgt" < "$OUT/test/valid.out" > "$OUT/test/valid.tc.bleu"
-perl "$TF/tools/multi-bleu.perl" -lc "$OUT/data/valid.tgt" < "$OUT/test/valid.out" > "$OUT/test/valid.lc.bleu"
+#echo "Training command :: $CMD"
+#eval "$CMD"
+#
+#echo "BPE decoding/detokenising target to match with references"
+#mv $OUT/test/test.out{,.bpe}
+#mv $OUT/test/valid.out{,.bpe}
+#cat $OUT/test/valid.out.bpe | sed -E 's/(@@ )|(@@ ?$)//g' > "$OUT/test/valid.out"
+#cat $OUT/test/test.out.bpe | sed -E 's/(@@ )|(@@ ?$)//g' > "$OUT/test/test.out"
+#
+#echo "Step 4a: Evaluate Test"
+#perl "$TF/tools/multi-bleu.perl" "$OUT/data/test.tgt" < "$OUT/test/test.out" > "$OUT/test/test.tc.bleu"
+#perl "$TF/tools/multi-bleu.perl" -lc "$OUT/data/test.tgt" < "$OUT/test/test.out" > "$OUT/test/test.lc.bleu"
+#
+#echo "Step 4b: Evaluate Dev"
+#perl "$TF/tools/multi-bleu.perl" "$OUT/data/valid.tgt" < "$OUT/test/valid.out" > "$OUT/test/valid.tc.bleu"
+#perl "$TF/tools/multi-bleu.perl" -lc "$OUT/data/valid.tgt" < "$OUT/test/valid.out" > "$OUT/test/valid.lc.bleu"
 
 t2t-bleu --translation="$OUT/test/test.out" --reference="$OUT/data/test.tgt" > "$OUT/test/test.t2t.bleu"
